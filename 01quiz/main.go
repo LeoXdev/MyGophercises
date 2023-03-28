cd25bcc772ca53b626978c8615a5d2f44dba3f73
Leonardo Antonio Tovíaz López <95661786+LeoXdev@users.noreply.github.com>
2023-03-28 10:28:54 -0600

    Updated 01quiz
    - Encapsulated two parts of the main logic into functions.
    - Added a 'quizzes' folders where all quiz files will reside.
    - You can choose which quiz from the 'quizzes' folder you want to tackle via a flag variable, usage: -filename=hangman.csv

--- 01quiz/main.go
+++ 01quiz/main.go
@@ -7,37 +7,34 @@ import (
 	"io"
 	"bufio"
 	"os"
+	"flag"
 )
 
 func main() {
 	var (
 		// quiz's hits and misses counter
 		hits, misses int = 0, 0
+
+		// Flag variables
+		filename *string = flag.String("filename", "problems.csv", "choose quiz file inside quizzes folder")
 	)
+	flag.Parse()
 
 	// open csv to read it and defer its closing
-	file, err := os.Open("problems.csv")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	file := OpenFile("quizzes/" + *filename) // !TODO: change the filename literal for a flag variable
 	defer file.Close()
 
 	csvReader := csv.NewReader(file)
 	for {
-		record, err := csvReader.Read()
-		// double error check as Read() may return different errors
-		if err == io.EOF {
+		record := ReadCSVRecord(csvReader)
+		// in case no more records are left
+		if record == nil {
 			break
-		}
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
 		}
 
 		// csv records processing,
 		// first field is always a question
-		// second field is always the answewr to the question
+		// second field is always the answer to the question
 		fmt.Printf("%v", record[0])
 		fmt.Print("=? ")
 		reader := bufio.NewReader(os.Stdin)
@@ -62,9 +59,34 @@ func main() {
 	os.Exit(0)
 }
 
-func ReadFile() {
+// OpenFile opens a file and returns a pointer to it, doesn't return an error
+// as the method itself manages it.
+func OpenFile(filepath string) *os.File {
+	file, err := os.Open(filepath)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
+	return file
 }
-func ReadCSVRecord(r csv.Reader) {
 
+// ReadCSVRecord reads a single line of a CSV file for each call, it receives
+// a pointer instead of a copy since Reader object has fields that may be
+// modified by the client application.
+func ReadCSVRecord(r *csv.Reader) []string {
+	record, err := r.Read()
+
+	// double error check as Read() may return different errors
+	if err == io.EOF {
+		// without this check, an error will be thrown and the program will
+		// shut down when reaching the end of the CSV
+		return nil
+	}
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	return record
 }

e84bcd008727e11927d1642d4d8a84055dde9dd5
Leonardo Antonio Tovíaz López <95661786+LeoXdev@users.noreply.github.com>
2023-05-03 10:12:08 -0600

    Added 02url bonus objective
    Added bool flag to either use a hardcoded yaml or an external file.

--- 02url/main.go
+++ 02url/main.go
@@ -1,23 +1,29 @@
+// The server may not start if port :8080 still has a process running, to kill it
+// enter 'fuser -k 8080/tcp' in terminal
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
+
+	"io/ioutil"
 
 	"02url/handler"
 )
 
+var (
+	useFile *bool = flag.Bool("usefile", false, "use file at path datafiles/data.yaml ?")
+)
+
 func main() {
-	var yamlData string = `
-path: /google
-url: https://www.google.com/
-`
+	flag.Parse()
 
 	// create fallback handler
 	mux := http.DefaultServeMux
 	mux.HandleFunc("/hello", hello)
 
-	// MapHandler implementation
+	// ------ MapHandler implementation ------
 	//pathsToUrls := map[string]string {
 	//	"/go" : "https://go.dev/",
 	//}
@@ -25,8 +31,22 @@ url: https://www.google.com/
 	// open server with both paths, to redirect and fallback handler
 	//http.ListenAndServe(":8080", handler.MapHandler(pathsToUrls, mux))
 
-	// YAMLhandler implementation
-	yamlHandler, err := handler.YAMLHandler([]byte(yamlData), mux)
+	// ------ YAMLhandler implementation ------
+	var data []byte
+	if (*useFile) {
+		yamlFile, err := ioutil.ReadFile("datafiles/data.yaml")
+		if err != nil {
+			panic(err)
+		}
+
+		data = yamlFile
+	} else {
+		data = []byte(`
+path: /google
+url: https://www.google.com/
+`)
+	}
+	yamlHandler, err := handler.YAMLHandler(data, mux)
 	if err != nil {
 		panic(err)
 	}

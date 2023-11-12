98be68904524aa835f5e38e34223887cf329186f
Leonardo Antonio Tovíaz López <toviaz.leonardo@gmail.com>
2023-11-12 12:53:35 -0600

    01quiz bonus objectives complete, enhanced comments

--- 01quiz/main.go
+++ 01quiz/main.go
@@ -5,10 +5,11 @@ import (
 	"encoding/csv"
 	"io"
 	"bufio"
-	"strings"
 	"os"
 	"flag"
 	"time"
+	"strings"
+	"math/rand"
 )
 
 var (
@@ -19,22 +20,27 @@ var (
 
 	filename *string = flag.String("filename", "problems.csv", "choose quiz file inside quizzes folder")
 	timer *int = flag.Int("timer", 30, "choose timer duration")
+	shuffle *bool = flag.Bool("shuffle", false, "shuffle quiz order?")
 )
 
-// !TODO: Add string trimming/cleaning (delete all blank spaces, no answer will ever have blank spaces)
 func main() {	
 	flag.Parse()
 
-	// add time limit to quizz
-	AddTimeLimit(*timer)
+	// add time limit to the quiz
+	addTimeLimit(*timer)
+
+	// shuffle if bool flag is true, panics if err occurs
+	if *shuffle {
+		shuffleCsv()
+	}
 
 	// open csv to read it and defer its closing
-	file := OpenFile("quizzes/" + *filename)
+	file := openFile("quizzes/" + *filename)
 	defer file.Close()
-
 	csvReader := csv.NewReader(file)
 	for {
-		record := ReadCSVRecord(csvReader)
+		// a record is a single line of the quiz
+		record := readCSVRecord(csvReader)
 		// in case no more records are left
 		if record == nil {
 			break
@@ -46,17 +52,19 @@ func main() {
 		fmt.Printf("%v", record[0])
 		fmt.Print("=? ")
 		reader := bufio.NewReader(os.Stdin)
-		// ReadString will block until the delimiter is entered
+		// ReadString will read until the delimiter is entered
+		// input includes the read delimiter
 		input, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Println("An error occured while reading input. Please try again", err)
 			return
 		}
-		// remove the delimeter from the string, ReadString returns a value up to the ocurring parameter
+		// remove the delimeter from the string
 		input = strings.TrimSuffix(input, "\n")
 		// remove white spaces
-		input = CleanAnswer(input)
+		input = cleanAnswer(input)
 
+		// check if answer was right
 		if input == record[1] {
 			hits++
 		} else {
@@ -64,14 +72,13 @@ func main() {
 		}
 	}
 
-
 	fmt.Printf("\nQuiz ended, hits: %v, misses:%v\n", hits, misses)
 	os.Exit(0)
 }
 
-// OpenFile opens a file and returns a pointer to it, doesn't return an error
+// openFile opens a file and returns a pointer to it, doesn't return an error
 // as the method itself manages it.
-func OpenFile(filepath string) *os.File {
+func openFile(filepath string) *os.File {
 	file, err := os.Open(filepath)
 	if err != nil {
 		fmt.Println(err)
@@ -81,12 +88,12 @@ func OpenFile(filepath string) *os.File {
 	return file
 }
 
-// ReadCSVRecord reads a single line of a CSV file for each call (using *csv.Reader.Read()),
+// readCSVRecord reads a single line of a CSV file for each call (using *csv.Reader.Read()),
 // it receives a pointer instead of a copy since Reader object has fields that may be
 // modified by the client application.
 // Returns nil if an io.EOF err is returned by reading a line of the csv, if
 // another type of error is returned by reading a line, the function exits the program.
-func ReadCSVRecord(r *csv.Reader) []string {
+func readCSVRecord(r *csv.Reader) []string {
 	record, err := r.Read()
 
 	// double error check as Read() may return different errors
@@ -103,16 +110,17 @@ func ReadCSVRecord(r *csv.Reader) []string {
 	return record
 }
 
-// AddTimeLimit adds a time limit to the quiz and ends it when the amount of time
+// addTimeLimit adds a time limit to the quiz and ends it when the amount of time
 // specified has elapsed, halting the current quiz's question and showing player's
 // final stats.
-func AddTimeLimit(amount int) {
+func addTimeLimit(amount int) {
 	// AfterFunc triggers a callback after n seconds
 	time.AfterFunc(time.Second * time.Duration(amount), func (){
-		file := OpenFile("quizzes/" + *filename)
+		file := openFile("quizzes/" + *filename)
 		defer file.Close()
 		
-		SkipNLines(file, hits + misses, func() {
+		// sum of hits + misses equals to the total answered questions
+		skipNLines(file, hits + misses, func() {
 			misses++
 		})
 
@@ -120,27 +128,69 @@ func AddTimeLimit(amount int) {
 		os.Exit(0)
 	})
 }
-// SkipNLines skips n lines from a file and calls a given function on all the
+// skipNLines skips n lines from a file and calls a given function on all the
 // non-skipped lines.
-func SkipNLines(file *os.File, n int, fx func()) {
+func skipNLines(file *os.File, n int, fx func()) {
 	csvReader := csv.NewReader(file)
 	lineCount := 0
 	for {
 		// get a line's record and store it
-		record := ReadCSVRecord(csvReader)
+		record := readCSVRecord(csvReader)
 		if record == nil {
 			return
 		}
 
-		// call the indicated funtion after n line skips
+		// call the indicated function after n line skips
 		lineCount++
 		if lineCount > n {
 			fx()
 		}
 	}
 }
-// CleanAnswer removes white spaces inside an entered answer, the strings
-// wouldn't need more processing than that.
-func CleanAnswer(str string) string {
+// cleanAnswer removes white spaces inside an entered answer, the strings
+// wouldn't need anymore processing.
+func cleanAnswer(str string) string {
 	return strings.ReplaceAll(str, " ", "")
+}
+// shuffleCsv shuffles the quiz using the modern version of Fisher-Yates
+// algorithm.
+func shuffleCsv() {
+	file := openFile("quizzes/" + *filename)
+	defer file.Close()
+	csvReader := csv.NewReader(file)
+
+	csvData, err := csvReader.ReadAll()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(csvData)
+	// Fisher-Yates algorithm for shuffling lines of the csvData
+	n := len(csvData) - 1
+	rand.Seed(time.Now().UnixNano())
+	for i := n; i > 0; i-- {
+		// select a random index from the unshuffled part of the csvData
+		// adding 1 to i leaves the possibility of an reciprocal exchange
+		j := rand.Intn(i + 1)
+		csvData[i], csvData[j] = csvData[j], csvData[i]
+	}
+	fmt.Println(csvData)
+
+	// os.Create will truncate selected quiz file
+	file, err = os.Create("quizzes/" + *filename)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// attempt to write shuffled lines into file
+	err = writer.WriteAll(csvData)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
